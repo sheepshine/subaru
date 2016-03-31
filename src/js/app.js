39a5d41d6c01287c05034eb0bc6a5387aaafe5a0
yangchaoben <842293890@qq.com>
2016-03-31 15:47:19 +0800

    第二页部分动画

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 /*汽车运动*/
 function sec(){
 	setTimeout(function(){
-		$(".carmove").addClass("carmove2")
+		$(".carmove").addClass("carmove2");
+		$(".zh").addClass("carmove2");
+		$(".xzline").addClass("carmove2");
 	},2000)
 }
