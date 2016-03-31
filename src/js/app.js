1a6b49aaf103983bcf191a00a965b001de3c4a34
yangchaoben <842293890@qq.com>
2016-04-01 01:19:24 +0800

    部分动画

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,57 @@ function sec(){
 		$(".xzline").addClass("carmove2");
 		setTimeout(function(){
 			$(".xz").addClass("carmove2");
+			$(".carmove").addClass("carmove3");
+			setTimeout(function(){
+				$(".xz").addClass("carmove3")
+				$(".carmove").removeClass("carmove3");
+				$(".carmove").addClass("carmove4");
+				setTimeout(function(){
+					$(".carmove").removeClass("carmove4");
+					$(".carmove").addClass("carmove5");
+					$(".fylline").addClass("carmove4")
+					setTimeout(function(){
+						$(".fyl").addClass("carmove5")
+						setTimeout(function(){
+							$(".fyl").removeClass("carmove5")
+							$(".carmove").removeClass("carmove5");
+							$(".carmove").addClass("carmove6");
+							$(".ashline").addClass("carmove5");
+							setTimeout(function(){
+								$(".ahh").addClass("carmove6")
+								setTimeout(function(){
+									$(".ahh").removeClass("carmove6")
+									$(".carmove").removeClass("carmove6");
+									$(".carmove").addClass("carmove7");
+									$(".mzdline").addClass("carmove6");
+									setTimeout(function(){
+										$(".msh").addClass("carmove7")
+										setTimeout(function(){
+											$(".msh").removeClass("carmove7")
+											$(".carmove").removeClass("carmove7");
+											$(".carmove").addClass("carmove8");
+											$(".qxlhline").addClass("carmove8");
+											setTimeout(function(){
+												$(".qxlh").addClass("carmove8")
+												setTimeout(function(){
+													$(".qxlh").removeClass("carmove8")
+													$(".carmove").removeClass("carmove8");
+													$(".carmove").addClass("carmove9");
+													$(".toahhline").addClass("carmove9");
+													setTimeout(function(){
+														$(".carmove").removeClass("carmove9");
+														$(".carmove").addClass("carmove10");
+													},1000)
+												},2000)
+											},1000)
+										},2000)
+									},1000)
+								},2000)
+							},1000)
+						},2000)
+					},1000)
+				},1000)
+			},2000)
 		},1000)
 		
 	},2000)
@@ -28,14 +79,63 @@ $(".join").click(function(){
         alert("请输入正确的手机号码");
         return false
     }
-	$("#info").hide()
-	$("#carSelect").show()
+    /*提交数据*/
+    $.ajax({
+	  url: url,
+	  dataType: 'json',
+	  data: {
+	  	name:$("#theName").val(),
+	  	phone:$("#thePhone").val()
+	  },
+	  success: function(res){
+	  	switch (res.code){
+	  		case 0:
+		  		/*不在名单内*/
+		  		alert("对不起，您不是斯巴鲁华南地区3月份的购车用户，无法参加该活动")
+		  		break;
+		  	case 1:
+		  		/*已经抽过奖*/
+		  		$("#noreward").show();  //没中奖显示
+		  		$("#reward").show();  //中奖显示
+		  		break;
+		  	case 2:
+		  		// 符合条件第一次抽奖用户
+		  		$("#info").hide();
+				$("#carSelect").show();
+				break;
+	  	}
+	  	
+	  }
+	});
+	
 })
-$(".selectcar").eq(1).click(function(){
-	$("#reward").show()
-})
-$(".selectcar").eq(2).click(function(){
-	$("#noreward").show()
+// 抽奖
+$(".selectcar").click(function(){
+	$.ajax({
+	  url: url,
+	  dataType: 'json',
+	  data: {
+	  	name:$("#theName").val(),
+	  	phone:$("#thePhone").val()
+	  },
+	  success: function(res){
+	  	switch (res.code){
+	  		case 1:
+		  		/*已经中过奖*/
+		  		$("#reward").show();
+		  		break;
+		  	case 1:
+		  		/*没中奖*/
+		  		$("#noreward").show();  //没中奖显示
+		  		break;
+		  	case 2:
+		  		// 中奖
+		  		$("#reward").show();
+				break;
+	  	}
+	  	
+	  }
+	});
 })
 $(".knowBtn").click(function(){
 	$("#QRcode").show()
