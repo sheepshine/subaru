273fe2f9adb5bf59cf51784e6f674c2b1e3ce468
yangchaoben <842293890@qq.com>
2016-03-31 20:25:43 +0800

    流程

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,43 @@ function sec(){
 		
 	},2000)
 }
+
+/*参加活动*/
+$(".join").click(function(){
+	if(!$("#theName").val()){
+		alert("请输入您的名字");
+		return false;
+	}
+	if(!$("#thePhone").val()){
+		alert("请输入您的电话")
+		return false;
+	}
+	var tel=$("#thePhone").val()
+	var telReg = !!tel.match(/^(0|86|17951)?(13[0-9]|15[012356789]|17[0678]|18[0-9]|14[57])[0-9]{8}$/);
+    //如果手机号码不能通过验证
+    if(telReg == false){
+        alert("请输入正确的手机号码");
+        return false
+    }
+	$("#info").hide()
+	$("#carSelect").show()
+})
+$(".selectcar").eq(1).click(function(){
+	$("#reward").show()
+})
+$(".selectcar").eq(2).click(function(){
+	$("#noreward").show()
+})
+$(".knowBtn").click(function(){
+	$("#QRcode").show()
+})
+$(".rankbtn").click(function(){
+	$(".rankwarp").show()
+})
+$(".rankwarp").click(function(){
+	
+	$(".rankwarp").hide()
+})
+$(".ranklist").click(function(event){
+	event.stopPropagation();
+})
